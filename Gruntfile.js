c9ed7dbb71c77803ba5ce98ced9f3ed2081787fe
mu <muflexnet@gmail.com>
2014-05-13 13:27:04 +0100

    buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -289,7 +289,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'git@github.com:mudev/muHyde.git',
           branch: 'gh-pages',
           commit: true,
           push: true
